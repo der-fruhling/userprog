b54bf740ce4798251329eb1eed040d1acfff59c0
Liam Cole <liam@dhcp15.jackalope>
2019-10-22 15:58:08 -0700

    Add lib.rb

---
+++ lib.rb
@@ -0,0 +1,16 @@
+require 'fileutils'
+
+def http(url, output)
+    system "wget -O tmp.tgz #{url}"
+    system "tar -zxpf tmp.tgz"
+    system "make -C tmp output.bin"
+    FileUtils.mv 'tmp/output.bin' "img/#{output}"
+    FileUtils.rm_r 'tmp'
+end
+
+def github(repo, output)
+    system "git clone https://github.com/#{repo} tmp"
+    system "make -C tmp output.bin"
+    FileUtils.mv 'tmp/output.bin' "img/#{output}"
+    FileUtils.rm_r 'tmp'
+end
