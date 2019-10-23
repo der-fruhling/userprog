7edd96aaff8463e45e20feefb9841e2ee674bb97
Liam Cole <liam@dhcp15.jackalope>
2019-10-23 15:04:27 -0700

    Liam has forgotten 2 commas

--- lib.rb
+++ lib.rb
@@ -4,13 +4,13 @@ def http(url, output)
     system "wget -O tmp.tgz #{url}"
     system "tar -zxpf tmp.tgz"
     system "make -C tmp output.bin"
-    FileUtils.mv 'tmp/output.bin' "img/#{output}"
+    FileUtils.mv 'tmp/output.bin', "img/#{output}"
     FileUtils.rm_r 'tmp'
 end
 
 def github(repo, output)
     system "git clone https://github.com/#{repo} tmp"
     system "make -C tmp output.bin"
-    FileUtils.mv 'tmp/output.bin' "img/#{output}"
+    FileUtils.mv 'tmp/output.bin', "img/#{output}"
     FileUtils.rm_r 'tmp'
 end
