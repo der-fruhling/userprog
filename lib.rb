1bc6ace3d7b4ef14420573d8208ebc80e6d473fa
Liam Cole <liam@liamiam.com>
2019-10-23 20:16:14 -0700

    Make it colourful!

--- lib.rb
+++ lib.rb
@@ -1,5 +1,18 @@
 require 'fileutils'
 
+def name_from_url(url)
+    a = url.split('/')
+    a[a.size - 1].split('.')[0]
+end
+
+def print_name_start(url)
+    puts "\e[32;1mStarting install of #{name_from_url(url)}\e[0m"
+end
+
+def print_name_end(url)
+    puts "\e[32;1mInstalled #{name_from_url(url)}\e[0m"
+end
+
 def http(url, output)
     system "wget -O tmp.tgz #{url}"
     system "tar -zxpf tmp.tgz"
