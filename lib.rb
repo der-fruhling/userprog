e7135e35a204d78fb8273de9bf8e3be407b0f6a6
Liam Cole <liam@liamiam.com>
2019-10-23 20:05:09 -0700

    Liam was doing a terrable thing...

--- lib.rb
+++ lib.rb
@@ -3,14 +3,14 @@ require 'fileutils'
 def http(url, output)
     system "wget -O tmp.tgz #{url}"
     system "tar -zxpf tmp.tgz"
-    system "make -C tmp output.bin"
+    system "make -C tmp"
     FileUtils.mv 'tmp/output.bin', "img/#{output}"
     FileUtils.rm_r 'tmp'
 end
 
 def github(repo, output)
     system "git clone https://github.com/#{repo} tmp"
-    system "make -C tmp output.bin"
+    system "make -C tmp"
     FileUtils.mv 'tmp/output.bin', "img/#{output}"
     FileUtils.rm_r 'tmp'
 end
